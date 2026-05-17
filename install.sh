36e56a47d98dfec117beb822a4753566a5330348
Zic <git@zicstardust.com>
2026-05-17 18:59:39 -0400

    fix name

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-zip -r com.zicstardust.activelinux.plasmoid com.zicstardust.activelinux/*
-#tar -C ./src -czf com.zicstardust.activelinux.plasmoid .
+zip -r com.zicstardust.activatelinux com.zicstardust.activatelinux/*
+#tar -C ./src -czf com.zicstardust.activatelinux.plasmoid .
 
 
-if [ -d "$HOME/.local/share/plasma/plasmoids/com.zicstardust.activelinux" ]; then
-    kpackagetool6 --type Plasma/Applet -u com.zicstardust.activelinux.plasmoid
+if [ -d "$HOME/.local/share/plasma/plasmoids/com.zicstardust.activatelinux" ]; then
+    kpackagetool6 --type Plasma/Applet -u com.zicstardust.activatelinux.plasmoid
 else
-    kpackagetool6 --type Plasma/Applet -i com.zicstardust.activelinux.plasmoid
+    kpackagetool6 --type Plasma/Applet -i com.zicstardust.activatelinux.plasmoid
 fi
 
 #plasmashell --replace &
